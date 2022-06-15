203e27a06b506c665838f600dc7cb99fbe07c3bc
anahita83radfar <anahita83radfar@gmail.com>
2022-06-15 15:08:28 +0000

    Creating the first view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Anahita',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 27, 2022'
+    },
+    {
+        'author': 'Elmira',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2021'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
